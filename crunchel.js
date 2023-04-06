1e092cf0853bb817a2a04ee201337200542ad199
GavinGoGaming <gavingogaming@gmail.com>
2023-04-05 19:50:13 -0700

    Got it working

--- crunchel.js
+++ crunchel.js
@@ -1,54 +1,93 @@
+var defaultFrames = {
+    walkingAnimation: {
+        lookingLeft: '/assets/player/ExamplePlayer.png',
+        lookingRight: '/assets/player/ExamplePlayer.png',
+        lookingUp: '/assets/player/ExamplePlayer.png',
+        lookingDown: '/assets/player/ExamplePlayer.png'
+    },
+    defaultImage: '/assets/player/ExamplePlayer.png'
+}
+
+function isOutside(element, container) {
+    if(element.getBoundingClientRect().top < container.getBoundingClientRect().top || 
+    element.getBoundingClientRect().bottom > container.getBoundingClientRect().bottom || 
+    element.getBoundingClientRect().left < container.getBoundingClientRect().left || 
+    element.getBoundingClientRect().right > container.getBoundingClientRect().right){
+        return true;
+    }
+    return false;
+}
+
 class Player {
-    element;
-    movement = false;
-    x = 0;
-    y = 0;
-    movementOptions = [];
-    constructor(element) {
+    constructor(element, options) {
+        this.listeners = {onmovement: []};
         this.element = element;
+        this.options = options;
+        this.data = {
+            collectedPoints: 0
+        };
     }
-    updateX(x) {
-        this.x = x;
-        this.element.style.top = this.y;
-        this.element.style.left = this.x;
-    }
-    updateY(y) {
-        this.y = y;
-        this.element.style.top = this.y;
-        this.element.style.left = this.x;
-    }
-    enableMovement(keybinds, options) {
-        this.movement = true;
-        this.movementOptions.push({
-            keybinds,
-            options
-        });
-        this.element.addEventListener("keydown", function(event) {
-            switch(event.key) {
-                case keybinds.up:
-                    event.target.style.top += 30;
+
+    enableBasicMovement(keys, options, frames=defaultFrames) {
+        const speed = options.speed || 10; // default speed is 10 if not provided
+        document.addEventListener('keydown', (event) => {
+            switch (event.key) {
+                case keys.up:
+                    
+                    this.callMoveEventListener(keys.up, this, this.element);
+                    var newStyle = (parseInt(this.element.style.top || 0) - speed);
+                    if(isOutside(this.element, this.element.parentNode)){
+                        newStyle = parseInt(this.element.style.left || 0)+speed;
+                    }
+                    this.element.style.top = `${newStyle}px`;
                     break;
-                case keybinds.down:
-                    event.target.style.top -= 30;
+                case keys.down:
+                    this.callMoveEventListener(keys.down, this, this.element);
+                    var newStyle = (parseInt(this.element.style.top || 0) + speed);
+                    if(isOutside(this.element, this.element.parentNode)){
+                        newStyle = parseInt(this.element.style.left || 0)-speed;
+                    }
+                    this.element.style.top = `${newStyle}px`;
                     break;
-                case keybinds.left:
-                    event.target.style.left += 30;
+                case keys.left:
+                    this.callMoveEventListener(keys.left, this, this.element);
+                    var newStyle = (parseInt(this.element.style.left || 0) - speed);
+                    if(isOutside(this.element, this.element.parentNode)){
+                        newStyle = parseInt(this.element.style.left || 0)+speed;
+                    }
+                    this.element.style.left = `${newStyle}px`;
                     break;
-                case keybinds.right:
-                    event.target.style.left -= 30;
+                case keys.right:
+                    this.callMoveEventListener(keys.right, this, this.element);
+                    var newStyle = (parseInt(this.element.style.left || 0) + speed);
+                    if(isOutside(this.element, this.element.parentNode)){
+                        newStyle = parseInt(this.element.style.left || 0)-speed;
+                    }
+                    this.element.style.left = `${newStyle}px`;
+                    break;
+                default:
                     break;
             }
         });
-        console.log("Enabled movement on player (id " +this.element.id+")");
+    }
+
+    callMoveEventListener(key, player, element) {
+        for(var listener of this.listeners.onmovement) {
+            listener({key, crunchTarget: player, target: element});
+        }
+    }
+    addMoveEventListener(callback) {
+        this.listeners.onmovement.push(callback);
     }
 }
-function crunch(htmlElement) {
-    switch(htmlElement.getAttribute('type')) {
-        case 'player':
-            return new Player(htmlElement);
-        case 'collectable':
-            return new Collectable(htmlElement);
+
+function crunch(element, options) {
+    switch(element.getAttribute('type')) {
+        case "player":
+            return new Player(element, options);
+        case "collectable point":
+            return new Point(element, options);
         default:
-            throw new Error("Unknown crunchel element type");
+            console.error("unrecognized type: " + element.getAttribute('type'));
     }
-}
+}  
