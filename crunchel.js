2fd7639eabf319fb61828824f82c95b034ae0735
GavinGoGaming <57292172+GavinGoGaming@users.noreply.github.com>
2023-04-06 19:15:41 -0700

    Add coins

--- crunchel.js
+++ crunchel.js
@@ -17,10 +17,37 @@ function isOutside(element, container) {
     }
     return false;
 }
+function isTouching(element1, element2) {
+    const rect1 = element1.getBoundingClientRect();
+    const rect2 = element2.getBoundingClientRect();
+
+    const touching = !(
+    rect1.bottom < rect2.top ||
+    rect1.top > rect2.bottom ||
+    rect1.right < rect2.left ||
+    rect1.left > rect2.right
+    );
+    return touching;
+}
+function isTouchingClass(element1, className) {
+    const elements2 = document.querySelectorAll(`.${className}`);
+  
+    for (const element2 of elements2) {
+      const rect1 = element1.getBoundingClientRect();
+      const rect2 = element2.getBoundingClientRect();
+  
+      if (!(rect1.bottom < rect2.top || rect1.top > rect2.bottom || rect1.right < rect2.left || rect1.left > rect2.right)) {
+        return true;
+      }
+    }
+  
+    return false;
+  }
+  
 
 class Player {
     constructor(element, options) {
-        this.listeners = {onmovement: []};
+        this.listeners = {onmovement: [], oncollectpoint: []};
         this.element = element;
         this.options = options;
         this.data = {
@@ -28,12 +55,25 @@ class Player {
         };
     }
 
+    enableCoinCollect(coinElement, destroyOnCollect) {
+        document.addEventListener('keydown', (event)=>{
+            if(isTouching(this.element, coinElement)) {
+                this.data.collectedPoints = parseInt(this.data.collectedPoints)+parseInt(coinElement.getAttribute('value'));
+                this.callCollectPointListener(coinElement, this);
+                if(destroyOnCollect) {
+                    coinElement.parentNode.removeChild(coinElement);
+                }
+            }
+        });
+    }
+
     enableBasicMovement(keys, options, frames=defaultFrames) {
-        const speed = options.speed || 10; // default speed is 10 if not provided
+        const speed = options.speed || 5;
         document.addEventListener('keydown', (event) => {
+            var BACKUP_TOP = this.element.style.top;
+            var BACKUP_LEFT = this.element.style.left;
             switch (event.key) {
                 case keys.up:
-                    
                     this.callMoveEventListener(keys.up, this, this.element);
                     var newStyle = (parseInt(this.element.style.top || 0) - speed);
                     if(isOutside(this.element, this.element.parentNode)){
@@ -68,6 +108,10 @@ class Player {
                 default:
                     break;
             }
+            if(isTouchingClass(this.element, "wall_block")) {
+                this.element.style.left = `${BACKUP_LEFT}px`;
+                this.element.style.top = `${BACKUP_TOP}px`;
+            }
         });
     }
 
@@ -75,6 +119,14 @@ class Player {
         for(var listener of this.listeners.onmovement) {
             listener({key, crunchTarget: player, target: element});
         }
+    }
+    callCollectPointListener(pointElement, player) {
+        for(var listener of this.listeners.oncollectpoint) {
+            listener({player, pointElement});
+        }
+    }
+    addCollectPointListener(callback) {
+        this.listeners.oncollectpoint.push(callback);
     }
     addMoveEventListener(callback) {
         this.listeners.onmovement.push(callback);
@@ -85,8 +137,6 @@ function crunch(element, options) {
     switch(element.getAttribute('type')) {
         case "player":
             return new Player(element, options);
-        case "collectable point":
-            return new Point(element, options);
         default:
             console.error("unrecognized type: " + element.getAttribute('type'));
     }
